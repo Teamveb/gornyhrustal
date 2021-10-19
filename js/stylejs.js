143741886eb3b4d54c2ddbfa1e55eebf3afac7c4
Teamveb <87662562+Teamveb@users.noreply.github.com>
2021-10-19 10:37:43 +0300

    this is my lendos

---
+++ js/stylejs.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $('.menu-burger_header').click(function(){
+        $('.menu-burger_header').toggleClass('open-menu');
+        $('.header__nav').toggleClass('open-menu');
+    });
+})
